e0f3be8929ea82f10227c86444002e07eb877708
huaqiangj <huaqiangj@tujia.com>
2019-04-12 10:53:28 +0800

    add

--- src/views/pc/Home.vue
+++ src/views/pc/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-      pc index
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header">
+      <div class="logo"></div>
+    </div>
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home',
-  components: {
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
